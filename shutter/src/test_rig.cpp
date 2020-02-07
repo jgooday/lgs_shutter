bb7ac665663b5d704baac6fd4682982952043130
telpa <goodayjack@gmail.com>
2020-02-07 11:42:59 +1100

    Working

--- shutter/src/test_rig.cpp
+++ shutter/src/test_rig.cpp
@@ -1,22 +1,24 @@
 #include "Arduino.h"
 #include "AccelStepper.h"
 
-int dir_pin = 8;
-int step_pin = 7;
-int enable_pin = 12;
+int dir_pin = 2;
+int step_pin = 3;
+int enable_pin = 4;
 
 AccelStepper motor(1, step_pin, dir_pin);
 
 void setup() {
   Serial.begin(9600);
 
-  motor.setMaxSpeed(2200);
+  motor.setMaxSpeed(1000);
   motor.setAcceleration(500);
   motor.moveTo(7000);
 }
 
 void loop() {
   if (motor.distanceToGo() == 0){
+    Serial.print("Moving to ");
+    Serial.println(-motor.currentPosition());
     motor.moveTo(-motor.currentPosition());
   }
   motor.run();
